c7abceeef7080cfd44c3b773c97d7ac30e81a6fc
Katlyn Boches <katlynboches@gmail.com>
2021-09-12 12:39:48 -0600

    generate vaules for password function

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
+
+// Start by generating values of password function
+var enter;
+var confirmNumberofChars;
+var confirmCharSymbols;
+var confirmUppercaseChars;
+var confirmLowercaseChars;
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
+  
 }
 
 // Add event listener to generate button
