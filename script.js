3da160ecd9bc22cf80129cb3893a29cd20779003
Katlyn Boches <katlynboches@gmail.com>
2021-09-13 09:37:27 -0600

    Created if statement with users choices

--- script.js
+++ script.js
@@ -8,13 +8,13 @@ var confirmLowercaseChars;
 // Special symbols
 symbols = ["!“#$%&’()*+,-./:;<=>?@[\]^_`{|}"];
 //Numeric characters
-number = [1,2,3,4,5,6,7,8,9,0];
+number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 // Alphabetical characters
-charAlpha= ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+charAlpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 // Choices delcared outside of if statement
 var choices;
 // Convert letters to uppercase
-var toUpperCase = function (x){
+var toUpperCase = function (x) {
   return x.toUpperCase();
 };
 // Create variable for uppercase conversion
@@ -23,7 +23,7 @@ charAlpha2 = charAlpha.map(toUpper);
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 // Add event listener to generate button
-generateBtn.addEventListener("click", function(){
+generateBtn.addEventListener("click", function () {
   password = generatePassword();
   document.getElementById("password").placeholder = password;
 });
@@ -31,10 +31,10 @@ generateBtn.addEventListener("click", function(){
 function writePassword() {
   enter = window.prompt("How many characters would you like in your password? Choose between 2 and 128");
   // First if statement for user validation 
-  if(!enter){
+  if (!enter) {
     alert("This needs a value");
   }
-  else if(enter < 8 || enter > 128){
+  else if (enter < 8 || enter > 128) {
     // Validates the users chice and input
     // Start user input prompts
     enter = window.prompt("You must choose between 8 and 128");
@@ -44,13 +44,17 @@ function writePassword() {
     confirmCharSymbols = confirm("Will this contain special characters?");
     confirmUppercaseChars = confirm("Will this contain Uppercase letters?");
     confirmLowercaseChars = confirm("Will this contain Lowercase letters?");
-    };
+  };
+  // First if statement taking users choice to determine password outcome
+  // If statement for all positive outcomes
+  if (confirmCharSymbols && confirmNumberofCharacters && confirmUppercaseChars && confirmLowercaseChars) {
+
+    choices = symbols.concat(number, charAlpha, charAlpha2);{
   }
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  else if{
 
-  passwordText.value = password;
 
-  
+  }
+    
 }
 
